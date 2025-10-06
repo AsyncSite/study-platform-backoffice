a59b4a34d8968f501f477e808bc31961e10ada72
renechoi <renechoi90@gmail.com>
2025-10-06 21:15:13 +0900

    fix: Comment out PaymentTransactionManagement import and route until file is created

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import { NotificationProvider } from './contexts/NotificationContext';
 import NotiManagement from './pages/NotiManagement';
 import QueryDailyManagement from './pages/QueryDailyManagement';
-import PaymentTransactionManagement from './pages/PaymentTransactionManagement';
+// import PaymentTransactionManagement from './pages/PaymentTransactionManagement'; // TODO: 파일 생성 후 활성화
 
 function App() {
   return (
@@ -96,6 +96,7 @@ function App() {
                 </PrivateRoute>
               }
             />
+            {/* TODO: PaymentTransactionManagement 파일 생성 후 활성화
             <Route
               path="/payments"
               element={
@@ -106,6 +107,7 @@ function App() {
                 </PrivateRoute>
               }
             />
+            */}
             {/* Will add more routes */}
             </Routes>
           </AuthProvider>
